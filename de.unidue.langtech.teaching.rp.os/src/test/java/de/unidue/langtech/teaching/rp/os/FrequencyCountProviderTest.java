9d0adc123c9e54d1e04b3e84bcec18a027e69c8e
Onur <onurs3232@googlemail.com>
2015-11-18 15:35:51 +0100

    Updated Test

--- de.unidue.langtech.teaching.rp.os/src/test/java/de/unidue/langtech/teaching/rp/os/FrequencyCountProviderTest.java
+++ de.unidue.langtech.teaching.rp.os/src/test/java/de/unidue/langtech/teaching/rp/os/FrequencyCountProviderTest.java
@@ -1,20 +1,4 @@
-/*******************************************************************************
- * Copyright 2011
- * Ubiquitous Knowledge Processing (UKP) Lab
- * Technische Universität Darmstadt
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ******************************************************************************/
+
 package de.unidue.langtech.teaching.rp.os;
 
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngine;
@@ -34,11 +18,7 @@ import de.tudarmstadt.ukp.dkpro.core.api.frequency.provider.FrequencyCountProvid
 import de.tudarmstadt.ukp.dkpro.core.api.resources.DkproContext;
 import de.tudarmstadt.ukp.dkpro.core.frequency.resources.Web1TFrequencyCountResource;
 
-/**
- * Derived from TestFrequencyCountResourceTest
- * @author Onur
- *
- */
+
 public class FrequencyCountProviderTest extends JCasAnnotator_ImplBase
 {
 	
@@ -75,7 +55,6 @@ public class FrequencyCountProviderTest extends JCasAnnotator_ImplBase
                         Web1TFrequencyCountResource.PARAM_LANGUAGE, "de"
                 ));
 
-        // Check the external resource was injected
         AnalysisEngine ae = createEngine(desc);
         ae.process(ae.newJCas());
         
